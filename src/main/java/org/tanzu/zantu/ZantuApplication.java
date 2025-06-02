c273a053df84f1d284c06a3be06d596589dd097d
Corby <pcorby@vmware.com>
2025-06-01 20:07:41 -0500

    Initial commit

---
+++ src/main/java/org/tanzu/zantu/ZantuApplication.java
@@ -0,0 +1,13 @@
+package org.tanzu.zantu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZantuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZantuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tanzu/zantu/ZantuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tanzu.zantu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZantuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
